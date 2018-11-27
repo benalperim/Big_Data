fa66b8ad2f3e3b571b8ee47195083034d6ba3db2
Calvin Le <calvin.md.le@gmail.com>
2018-11-26 18:17:35 -0700

    Update testing.py
    When writing the CSV output, it will be the same format as the input.
    The testing still tests accuracy compared to the given labels.

--- NeuralNetwork/testing.py
+++ NeuralNetwork/testing.py
@@ -27,28 +27,33 @@ def build_model(trainData):
 if __name__ == "__main__":
     data_2018 = pandas.read_csv(sys.argv[1],
                                 names=['Title', 'Actor1', 'Actor2', 'Director', 'Studio', 'Share'])
+
     trainData2018, testData2018 = train_test_split(data_2018, test_size=0.2)
 
     trainLabels2018 = trainData2018.as_matrix(columns=['Share'])
     trainData2018 = trainData2018.as_matrix(columns=['Title', 'Actor1', 'Actor2', 'Director', 'Studio'])
     testLabels2018 = testData2018.as_matrix(columns=['Share'])
     testData2018 = testData2018.as_matrix(columns=['Title', 'Actor1', 'Actor2', 'Director', 'Studio'])
-    np.random.shuffle(trainData2018)
+
+    combinedData2018 = np.concatenate([trainData2018, testData2018])
+    combineShares2018 = np.concatenate([trainLabels2018, testLabels2018])
+    np.random.shuffle(combinedData2018)
 
     model = build_model(trainData2018)
     model.load_weights('./2017weights')
 
-    #TODO: Write all the features to the csv as well to show complete data
-    testPredictions2018 = np.absolute(model.predict(testData2018))
-    np.savetxt("predicted_2018_shares.csv", testPredictions2018, delimiter=",")
+    testPredictions2018 = np.absolute(model.predict(combinedData2018))
+    df = pandas.DataFrame(combinedData2018)
+    df_shares = pandas.DataFrame(testPredictions2018)
+    df['Shares'] = df_shares
+    print(df.shape)
+    df.to_csv('predicted_2018_shares.csv', sep=",", index=False, header=None)
 
-    # 2018 DATA - MEAN AVERAGE ERROR
-    # [loss, mae] = model.evaluate(testData2018, testLabels2018, verbose=0)
-    # print("Testing set Mean Abs Error: {:1.5f}".format(mae))
+    #2018 DATA - MEAN AVERAGE ERROR
+    [loss, mae] = model.evaluate(testData2018, testLabels2018, verbose=0)
+    print("Testing set Mean Abs Error: {:1.5f}".format(mae))
 
-    error2018 = testPredictions2018 - testLabels2018
-
-
+    error2018 = testPredictions2018 - combineShares2018
     plt.hist(error2018)
     plt.xlabel("2018 Shares Prediction Error")
     _ = plt.ylabel("Count")
